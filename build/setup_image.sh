62d20f6bd9436a85b0acc49f562c8d4f661d1787
Michael Salino-Hugg <msalinohugg@seas.harvard.edu>
2025-10-14 14:07:16 -0400

    lint

--- build/setup_image.sh
+++ build/setup_image.sh
@@ -30,7 +30,7 @@ time apt install "${APT_NONINTERACTIVE}" --fix-broken --fix-missing --no-upgrade
 	libpigpio-dev \
 	overlayroot \
 	pigpio \
-	rsyslog 
+	rsyslog
 
 apt "${APT_NONINTERACTIVE}" autoremove
 
@@ -91,7 +91,7 @@ tar -cf - -C "${OVERLAY_DIR}" --owner=pi --group=pi . | tar -xf - -C /
 time apt install "${APT_NONINTERACTIVE}" --fix-broken --fix-missing --no-upgrade \
 	dhcpcd5
 systemctl disable NetworkManager.service
-echo "hostname" >> /etc/dhcpcd.conf
+echo "hostname" >>/etc/dhcpcd.conf
 systemctl enable dhcpcd.service
 # ln -s /lib/systemd/system/dhcpcd.service /mnt/etc/systemd/system/multi-user.target.wants/dhcpcd.service
 {
